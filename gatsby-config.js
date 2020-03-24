fdc3a8f4f33c946f52023d44ce1065a2af8f662e
Ty <nguyencaotuonghieu@gmail.com>
2020-03-24 23:36:00 +0700

    Commit code

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/content/posts/blog`,
+        path: `${__dirname}/content/posts/`,
       },
     },
     {
